0ef679e2758f1541393e3d78532a587940254d33
Lyman Gillispie <lyman.gillispie@gmail.com>
2012-07-12 11:58:43 -0800

    fixed minor bugs in converter utility, moved to util folder

--- util/json_input/json_to_degree.py
+++ util/json_input/json_to_degree.py
@@ -48,7 +48,7 @@ def melt_mod_dict_to_dat(config):
     Returns: currently prints to screen
     TODO: return a strin to be output to a file or to the screen
     '''
-    first_part = '\n\n{daysscreenoutput}\n{inpath}\n{outpath}\n{jdbeg}  {yearbeg}\n{jdend}  {yearend}\n{disyes}\n{calcgridyes}\n\n{maxmeltstakes}\n{plusminus}\n{do_out}\n\n{shayes} {exkyes} {solyes} {diryes} {dir2yes} {difyes} {gloyes} {albyes} {swbyes} {linyes} {loutyes}\n\n{netyes} {senyes} {latyes} {raiyes} {enbyes} {melyes}  {ablyes} {surftempyes} {posyes} {ddfyes}\n{surfyes}\n{snowyes}\n{daysnow}\n{numbersnowdaysout}'
+    first_part = '\n\n{daysscreenoutput}\n{inpath}\n{outpath}\n{jdbeg}  {yearbeg}\n{jdend}  {yearend}\n{disyes}\n{calcgridyes}\n\n{maxmeltstakes}\n{plusminus}\n{do_out}\n\n{shayes} {exkyes} {solyes} {diryes} {dir2yes} {difyes} {gloyes} {albyes} {swbyes} {linyes} {loutyes}\n\n{netyes} {senyes} {latyes} {raiyes} {enbyes} {melyes} {ablyes} {surftempyes} {posyes} {ddfyes}\n{surfyes}\n{snowyes}\n{daysnow}\n{numbersnowdaysout}'
  
     print(first_part).format(**config)
 
@@ -62,7 +62,8 @@ def melt_mod_dict_to_dat(config):
     for grid in config['outgrids']:
         print (grid['name'] + " " + 
             " ".join(map(str, grid['location'])) + 
-            " " + str(grid['outglobnet']) + '\n')
+            " " + str(grid['outglobnet']))
+    print ""
 
     third_part = '{methodinisnow}\n{methodsnowalbedo}\n{methodglobal}\n{methodlonginstation}\n{methodlongin}\n{methodsurftempglac}\n\n{methodturbul}\n{method_z0Te}\n{methodiceheat}\n{methodnegbal}\n\n{scalingyes}\n{gamma}\n{c_coefficient}\n\n{namedgm}\n{namedgmdrain}\n{namedgmglac}\n{namedgmslope}\n{namedgmaspect}\n{namedgmskyview}\n{namedgmfirn}\n{nameinitialsnow}\n{nameklima}\n\n{laenge}\n{breite}\n{reflongitude}\n{rowclim}\n{colclim}\n{climoutsideyes}  {heightclim}\n{gridsize}\n{timestep}\n\n{formatclimdata}\n{maxcol}\n{coltemp}\n{colhum}\n{colwind}\n{colglob}\n{colref}\n{colnet}\n{collongin}\n{collongout}\n{colprec}\n{colcloud}\n{coltempgradvarying}\n{coldis}\n\n{ERAtempshift}\n{ERAwindshift}\n\n{methodtempinterpol}\n{tempgrad}\n{tempscenario}\n{precscenario}\n'
 
@@ -78,7 +79,7 @@ def melt_mod_dict_to_dat(config):
     " ".join(map(str,config["monthprecipscen"])))
     print ''
 
-    fourth_part ='{n_albfiles}\n{albsnow}\n{albslush}\n{albice}\n{albfirn}\n{albrock}\n{albmin}\n{snowalbincrease}\n{albiceproz}\n{ndstart}\n\n{split}\n{prozdiffuse}\n{trans}\n{ratio}\n{ratiodir2dir}\n{surftemplapserate}\n{directfromfile}\n{pathdirectfile}\n{daysdirect}\n{slopestation}\n\n{iterstep}\n{z0wice}\n{dividerz0T}\n{dividerz0snow}\n{z0proz}\n{icez0min:.10f}\n{icez0max}\n\n{methodprecipinterpol}\n{precgrad}\n{precgradhigh} {precgradelev}\n{preccorr}\n{snowmultiplierglacier}\n{snowmultiplierrock}\n{threshtemp}\n{onlyglacieryes}\n{glacierpart}\n\n{nameqcalc}\n{nodis}\n{firnkons}\n{snowkons}\n{icekons}\n{icekons}\n\n{qfirnstart}\n{qsnowstart}\n{qicestart}\n{qicestart}\n{qground}\n{jdstartr2diff}\n\n{disyesopt}\n{optkA}\n{startopt1}\n{stepopt1}\n{anzahlopt1}\n{optkB}\n{startopt2}\n{stepopt2}\n{anzahlopt2}\n{namematrix}\n\n{percolationyes}\n{slushformationyes}\n{densificationyes}\n{wetstartyes}\n{ndepths}\n{factinter}\n\n{thicknessfirst}\n{thicknessdeep}\n{depthdeep}\n{denssnow}\n{irrwatercontyes}\n{irrwatercont}\n\n{factsubsurfout}\n{offsetsubsurfout}\n\n{runoffyes} {superyes} {wateryes} {surfwateryes} {slushyes} {coldsnowyes} {coldtotyes}\n\n\n\n{ddmethod}\n{DDFice}\n{DDFsnow}\n\n{meltfactor}\n{radfactorice}\n{radfactorsnow}\n{debrisfactor}\n\n{coordinatesyes}'
+    fourth_part ='{n_albfiles}\n{albsnow}\n{albslush}\n{albice}\n{albfirn}\n{albrock}\n{albmin}\n{snowalbincrease}\n{albiceproz}\n{ndstart}\n\n{split}\n{prozdiffuse}\n{trans}\n{ratio}\n{ratiodir2dir}\n{surftemplapserate}\n{directfromfile}\n{pathdirectfile}\n{daysdirect}\n{slopestation}\n\n{iterstep}\n{z0wice}\n{dividerz0T}\n{dividerz0snow}\n{z0proz}\n{icez0min:.10f}\n{icez0max}\n\n{methodprecipinterpol}\n{precgrad}\n{precgradhigh} {precgradelev}\n{preccorr}\n{snowmultiplierglacier}\n{snowmultiplierrock}\n{threshtemp}\n{onlyglacieryes}\n{glacierpart}\n\n{nameqcalc}\n{nodis}\n{firnkons}\n{snowkons}\n{icekons}\n{rockkons}\n\n{qfirnstart}\n{qsnowstart}\n{qicestart}\n{qicestart}\n{qground}\n{jdstartr2diff}\n\n{disyesopt}\n{optkA}\n{startopt1}\n{stepopt1}\n{anzahlopt1}\n{optkB}\n{startopt2}\n{stepopt2}\n{anzahlopt2}\n{namematrix}\n\n{percolationyes}\n{slushformationyes}\n{densificationyes}\n{wetstartyes}\n{ndepths}\n{factinter}\n\n{thicknessfirst}\n{thicknessdeep}\n{depthdeep}\n{denssnow}\n{irrwatercontyes}\n{irrwatercont}\n\n{factsubsurfout}\n{offsetsubsurfout}\n\n{runoffyes} {superyes} {wateryes} {surfwateryes} {slushyes} {coldsnowyes} {coldtotyes}\n\n\n\n{ddmethod}\n{DDFice}\n{DDFsnow}\n\n{meltfactor}\n{radfactorice}\n{radfactorsnow}\n{debrisfactor}\n\n{coordinatesyes}'
 
     print(fourth_part).format(**config)
 
