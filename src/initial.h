68b8c9abfda416a07394ca6d06a205d31396478d
Lyman Gillispie <lyman.gillispie@gmail.com>
2012-07-25 14:00:07 -0800

    commented initial.h

--- src/initial.h
+++ src/initial.h
@@ -10,24 +10,133 @@
 
 #include<stdio.h>
 
+/*******************************************************/
+/* READ REST OF LINE                                   */
+/*******************************************************/
 void readrestofline(FILE **infile);
+
+
 void checkgridinputdata_ok();
+
+/************************************************************/
+/* EXIT PROGRAM IF TOO MANY LINES WRITTEN TO OUTPUT res.out */
+/*  to avoid that harddisk is filled to limit               */
+/*  called from mainly subsurf()                            */
+/*  resoutlines are added every time a variable exceeds preset limits */
+/*  e.g. if surftemperature would fall below given limit    */
+/************************************************************/
 void toobig_resout();
 
+/***********************************************/
+/*     CLOSE FILE                              */
+/***********************************************/
 void closefile(FILE **datei, char *name);
+
+/*============================================================================*/
+/*  FUNCTIONS FOR INITIALISATION OF OUTPUT FILES WITH COMPONENTS OF ENERGY BALANCE
+    DAILY MEANS AND/OR MEANS FOR THE WHOLE PERIOD
+       called at end of start and from main                                 ***/
+
+/*************************************************************/
+/* FUNCTION  meandayreserve
+	     RESERVE STORAGE FOR GRID ARRAYS FOR DAILY MEANS */
+/*************************************************************/
 void meandayreserve();
+
+/******************************************************/
+/* FUNCTION  meandaynull
+	     INITIALISATION OF ARRAYS FOR DAILY MEANS */
+/*must be zero, because of summing up  */
+/******************************************************/
 void meandaynull();
+
+/****************************************************************/
+/* FUNCTION  meanallnull
+	     RESERVE STORAGE FOR ARRAY
+	     INITIALISATION OF ARRAYS FOR MEANS OF WHOLE PERIOD
+	     called from start                                  */
+/****************************************************************/
 void meanallnull();
+
+/****************************************************************************/
+/*  FUNCTION  startinputdata                                                */
+/*    OPEN AND READ GRID FILES DTM, drainage basin, glacier, slope, aspect  */
+/*    OPEN FILE WITH CLIMATE DATA and READ UNTIL START                      */
+/****************************************************************************/
 void startinputdata();
+
+/*========================= OUTPUT ===========================================*/
+
+
+/*******************************************************************************/
+/*  FUNCTION  startoutascii                                                    */
+/*    OPEN ASCII-FILES FOR TIME SERIES OUTPUT (spatial means or station output)*/
+/*   and WRITE FIRST 2 ROWS                                                    */
+/*-----------------------------------------------------------------------------*/
 void startoutascii();
+
+/*******************************************************************************/
+/*  FUNCTION  startmeltstakes
+      OPEN FILE FOR ASCI-OUTPUT OF MELT FOR STAKE LOCATIONS
+      OUTPUT OF CUMULATIVE MELTING AND CUMULATIVE MASS BALANCE                 */
+/*******************************************************************************/
 void startmeltstakes();
+
+/*******************************************************************************/
+/*  FUNCTION  startspecificmassbalance
+      OPEN FILE FOR ASCI-OUTPUT OF SPECIFIC MASS BALANCE FOR EACH YEAR
+      ONLY IF summer and winter mass balance COMPUTED AND ONLY USEFUL FOR
+      MULTIYEAR RUNS;  CALLED FROM MAIN ONCE
+      NEW: 3/2004  */
+/*******************************************************************************/
 void startspecificmassbalance();
+
+/*******************************************************************************/
+/*  FUNCTION  opensnowfree                                                     */
+/*      OPEN FILE FOR TIME SERIES OF HOW MANY PIXELS ARE SNOW FREE EVERY DAY   */
+/*******************************************************************************/
 void opensnowfree();
+
+/*******************************************************************************/
+/*  FUNCTION  startarrayreserve
+      RESERVE STORAGE AND INITIALIZE OUTPUT-ARRAYS FOR GRID MODEL RESULTS      */
+/*******************************************************************************/
 void startarrayreserve();
+
+/*******************************************************************************/
+/*  FUNCTION  glacierrowcol                                                    */
+/*    FIND THE FIRST AND LAST ROW WITH NO MISSING VALUES IN THE DRAINAGE GRID  */
+/*    AND FIND FOR EACH ROW WITH DRAINAGE BASIN GRIDS THE FIRST AND LAST COLUMN*/
+/* to speed up the program, calculations are only done for basin grid cells    */
+/*******************************************************************************/
 void glacierrowcol();
+
+/**************************************************************/
+/*  FUNCTION  readclim                                        */
+/*        READ ONE LINE OF CLIMATE DATA FROM FILE             */
+/*          called from main every time step                  */
+/**************************************************************/
 void readclim();
+
+/********************************************************************/
+/*  FUNCTION  exitclimread                                          */
+/*       EXITS PROGRAM IF READ INPUT DATA IS OUTSIDE LOGICAL VALUES */
+/*          called from every time step                    */
+/********************************************************************/
 void exitclimread();
+
+/********************************************************************/
+/*  FUNCTION  readdatesmassbal                                      */
+/*       reads the dates of mass balance measurements               */
+/*          called from main once                                   */
+/********************************************************************/
 void readdatesmassbal();
+
+/********************************************************************/
+/*  FUNCTION  areaelevationbelts                                    */
+/*       counts the number of grid cells for each elevation band    */
+/*       called from main once,  May 2006                           */
+/********************************************************************/
 void areaelevationbelts();
 
 #endif /* MELT_MOD_INITIAL_H_ */
