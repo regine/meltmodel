7a0781ed60287ce81e6fc1d71566343caadc5754
Lyman Gillispie <lyman.gillispie@gmail.com>
2012-08-09 17:08:58 -0800

    added GLP and gpl headers to each C file. Need to add copyright notices

--- src/degree.c
+++ src/degree.c
@@ -1,10 +1,26 @@
-
+/***********************************************************************
+ * This file is part of MeltMod.
+ * 
+ * MeltMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MeltMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MeltMod.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
 /*********************************************************************/
 /* PROGRAM  degree.c                                                 */
 /*  DISTRIBUTED SNOW/ICE MELT MODEL BASED ON TEMPERATURE INDEX       */
 /*  METHODS ENERGY BALANCE INCLUDING OPTIONAL DISCHARGE CALCULATIONS */
 /*   5.3.1998, update 21 Sep 2011                                   */
 /*********************************************************************/
+
 
 #include<stdio.h>
 #include<stdlib.h>
